03a0a31b19d3223b33c47fbf5e98c7ea999026a6
melbaz231 <melbaz231@gmail.com>
2023-05-11 16:11:09 -0400

    campChoref v1

---
+++ index.js
@@ -0,0 +1,41 @@
+(function () {
+    'use strict';
+
+    const detailsDiv = document.getElementById('detailsDiv');
+    let formIsDisplayed = false;
+    const form = document.getElementById('JotFormIFrame-231155895577166');
+    const flyerDiv = document.getElementById('flyerDiv');
+    const detailsLink = document.getElementById('detailsLink');
+    let linkIsDisplayed = false;
+    const registerButton = document.getElementById('register');
+
+    registerButton.addEventListener('click', displayForm);
+    detailsLink.addEventListener('click', displayDetails);
+
+    function displayDetails() {
+        if (!linkIsDisplayed) {
+            detailsDiv.style.display = 'flex';
+            flyerDiv.style.display = 'none';
+            detailsLink.innerText = 'Hide Details';
+            linkIsDisplayed = true;
+        }
+        else {
+            detailsDiv.style.display = 'none';
+            flyerDiv.style.display = 'flex';
+            detailsLink.innerText = 'See Details';
+            linkIsDisplayed = false;
+        }
+    }
+    function displayForm() {
+        if (!formIsDisplayed) {
+            form.style.display = 'inline';
+            registerButton.innerText = 'Cancel';
+            formIsDisplayed = true;
+        }
+        else {
+            form.style.display = 'none';
+            registerButton.innerHTML = '<p>Click Here to <br> Register Now!</p>';
+            formIsDisplayed = false;
+        }
+    }
+})();
